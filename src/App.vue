0b8c1507e316c26fcb6ba0676dfc9f67babe82da
Brian Lee <brian@relion365.com>
2019-09-04 11:16:53 -0700

    9/4/19 11:16am

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,10 @@
   <form name="Equipment" method="POST" data-netlify="true">
     <div class="container">
       <div class="row">
-          <label>Your Name: <input type="text" name="name" /></label>   
+          <label>Customer: <input type="text" name="name" /></label>   
       </div>
       <div class="row">
-          <label>Your Email: <input type="email" name="email" /></label>
-      </div>
-      <div class="row">
-          <label>Your Role: <select name="role[]" multiple>
-          <option value="leader">Leader</option>
-          <option value="follower">Follower</option>
-          </select></label>
+          <label>Address: <textarea rows="4" cols="50"></textarea></label>
       </div>
       <div class="row">
        <table class="table">
